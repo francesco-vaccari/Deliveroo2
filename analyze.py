64709deda8fe63a9649eaabad0a2562faa7f2310
francesco-vaccari <francesco.vaccari@studenti.unitn.it>
2024-10-18 16:53:52 +0200

    Improved analysis tool

--- analyze.py
+++ analyze.py
@@ -23,8 +23,6 @@ for element in os.listdir(folder):
             agents_folders.append(os.path.join(folder, element, 'result'))
             agents_names.append(element)
 
-# files_to_analyze = ['analyzable_DTF.py', 'analyzable_IF_NW.py', 'analyzable_IF_W.py', 'analyzable_IF.py', 'analyzable_PF.py']
-files_to_analyze = []
 # folders_to_analyze = ['analyzable_DTF_S', 'analyzable_IF_NW_S', 'analyzable_IF_W_S', 'analyzable_IF_S', 'analyzable_PF_S']
 folders_to_analyze = ['analyzable_DTF_S', 'analyzable_IF_NW_S', 'analyzable_IF_W_S', 'analyzable_PF_S']
 
@@ -37,12 +35,9 @@ for i in range(len(agents_folders)):
         for file in os.listdir(os.path.join(agents_folders[i], folder)):
             if file.endswith('.py'):
                 files[agents_names[i]][folder].append(os.path.join(agents_folders[i], folder, file))
-    for file in files_to_analyze:
-        files[agents_names[i]][file] = [os.path.join(agents_folders[i], file)]
 
 
 def get_cc(file_path):
-    print(f"\t\tCC")
     os.system(f"radon cc -s -j {file_path} > temp.json")
     with open("temp.json", "r") as f:
         data = json.load(f)
@@ -57,13 +52,12 @@ def get_cc(file_path):
             ranks.append(function['rank'])
             complexities.append(function['complexity'])
             n_lines.append(function['endline'] - function['lineno'] + 1)
-            print(f"\t\t\t{function['name']:15}\t{function['rank']:5}\t{function['complexity']:5}\t{function['endline'] - function['lineno'] + 1:5}")
+            print(f"\t\tCC \tRank: {function['rank']}\tComplexity: {function['complexity']}\tLines: {function['endline'] - function['lineno'] + 1}")
     except:
         print(f"\t\t\tNo functions found in {file_path}")
     return (names, ranks, complexities, n_lines)
 
 def get_mi(file_path):
-    print(f"\t\tMI")
     with open(file_path, "r") as f:
         if f.read() == "":
             print(f"\t\t\tFile {file_path} is empty")
@@ -74,24 +68,21 @@ def get_mi(file_path):
         f.close()
     mi = data[file_path]['mi']
     rank = data[file_path]['rank']
-    print(f"\t\t\t{mi:5}\t{rank:5}")
+    print(f"\t\tMI \tMI: {mi}\tRank: {rank}")
     return (mi, rank)
 
 def get_raw(file_path):
-    print(f"\t\tRaw")
     os.system(f"radon raw -j {file_path} > temp.json")
     with open("temp.json", "r") as f:
         data = json.load(f)
         f.close()
-    
     lloc = data[file_path]['lloc']
     sloc = data[file_path]['sloc']
     comments = data[file_path]['comments']
-    print(f"\t\t\t{lloc:5}\t{sloc:5}\t{comments:5}")
+    print(f"\t\tRaw\tLLOC: {lloc}\tSLOC: {sloc}\tComments: {comments}")
     return (lloc, sloc, comments)
 
 def get_hal(file_path):
-    print(f"\t\tHal")
     os.system(f"radon hal -f -j {file_path} > temp.json")
     with open("temp.json", "r") as f:
         data = json.load(f)
@@ -110,7 +101,7 @@ def get_hal(file_path):
         efforts.append(metrics['effort'])
         times.append(metrics['time'])
         bugs.append(metrics['bugs'])
-        print(f"\t\t\t{function_name:15}\t{metrics['volume']:5}\t{metrics['difficulty']:5}\t{metrics['effort']:5}\t{metrics['time']:5}\t{metrics['bugs']:5}")
+        print(f"\t\tHal\tVolume: {metrics['volume']}\tDifficulty: {metrics['difficulty']}\tEffort: {metrics['effort']}\tTime: {metrics['time']}\tBugs: {metrics['bugs']}")
     return (names, volumes, difficulties, efforts, times, bugs)
 
 
@@ -119,12 +110,22 @@ print("-----------------------------------------------------")
 for agent, agent_files in files.items():
     print(f"Agent: {agent}")
     for file, path in agent_files.items():
-        print(f"\n\n* {file.split('.')[0]:15}\t\t{[file_path.split('/')[-1] for file_path in path]}")
+        if file.split('.')[0] == 'analyzable_DTF_S':
+            name = 'Desire Trigger Functions'
+        elif file.split('.')[0] == 'analyzable_IF_NW_S':
+            name = 'Intention Functions (Not Working)'
+        elif file.split('.')[0] == 'analyzable_IF_W_S':
+            name = 'Intention Functions (Working)'
+        elif file.split('.')[0] == 'analyzable_PF_S':
+            name = 'Perception Functions'
+        else:
+            name = file.split('.')[0]
+        print(f"\n* {name:15}\t\t{[file_path.split('/')[-1].split('.')[0] for file_path in path]}")
         if len(path) == 0:
             # print(f"\tNo files found in {file}")
             pass
         for file_path in path:
-            print(f'\t{file_path.split('/')[-1]}')
+            print(f'\t{file_path.split('/')[-1].split('.')[0]}')
             res = get_cc(file_path)
             res = get_mi(file_path)
             res = get_raw(file_path)
